e765c8096d518b826748ff7e90510e93c4fcc209
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-05-18 21:50:17 +0900

    ㅁ

--- src/section02/Variable05.java
+++ src/section02/Variable05.java
@@ -1,4 +1,7 @@
 package section02;
+
+import java.math.BigDecimal;
+
 /*
  * 데이터 표현방식
  * 정수표현
@@ -20,13 +23,21 @@ package section02;
 public class Variable05 {
 	public static void main(String[] args) {
 		float f = 0.0f;
+		double d = 0.0;
+		BigDecimal bd = new BigDecimal(0.0);
+		
 				//  for 반복문
 				for (int i=0; i <100; i=i+1) { // 100번 반복하라 (0부터 99까지)
 					f += 0.1f;// f = f+ 0.1f
+					d += 0.1d;
+					bd = bd.add(new BigDecimal(0.1));
+					
+					
 				}
 	System.out.println(f);
+	System.out.println(d);
+	System.out.println(bd);
 	
-		
 	}
 
 }
---
+++ src/section02/Variable06.java
@@ -0,0 +1,48 @@
+package section02;
+/*
+ * 5.String 형(문자열)
+ * 문자열을 저장할 수 있다.
+ * 참조형 변수
+ * 
+ * 선언방법
+ * String 변수명;
+ * 
+ * 기본형 - boolean, char, byte, short, int, long, float, double 
+ * 		 8가지
+ * 		실제 값을 저장
+ * 참조형 - 기본형을 제외한 모든 객체(클래스 코드로 되어있다)
+ * 		주소 값을 저장 
+ * 		속성(변수), 기능(메소드)  
+ * 
+ * 
+ * 
+ * 
+ */
+public class Variable06 {
+	public static void main(String[] args) {
+	//변수 선언과 동시에 대입하기
+		String str = "안녕하세요.";
+		System.out.println("str:" +str);
+		
+		/*
+		 * hello
+		 * 01234 <=인덱스번호
+		 */
+		
+		String hello = "hello";
+		System.out.println("1번 인텍스: "+ hello.charAt(01));
+		System.out.println("2번 인텍스부터 4번 인덱스까지: "+ hello.substring(2,4));
+		
+		String addr = "서울 특별시 서대문구 신촌";
+		boolean isContain = addr.contains("특별시");
+		
+		//if 조건문 
+		if(isContain) {
+			System.out.println("특별시민 입니다!!");
+		}
+		else {
+			System.out.println("특별시민이 아닙니다!");
+		}
+	}
+	}
+
